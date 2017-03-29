6e9e2e460b7763c8db0580ce8d7df9453b94af61
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2017-03-30 03:31:49 +0700

    generalization and make ejs/jquery

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('fronts/index.ejs', { title: 'Express' });
+  res.render('fronts/index.ejs', { page: 'home' });
 });
 router.get('/about', function(req, res, next) {
-  res.render('fronts/about.ejs', { title: 'Express' });
+  res.render('fronts/about.ejs', { page: 'about' });
 });
 router.get('/menu', function(req, res, next) {
-  res.render('fronts/burger.ejs', { title: 'Express' });
+  res.render('fronts/burger.ejs', { page: 'menu' });
 });
+// router.get('/menu/:menu_name', function(req, res, next) {
+//   var menu_name = req.params.menu_name;
+//   res.render('fronts/burger.ejs', { page: 'menu', menu_name: menu_name });
+// });
 router.get('/contact', function(req, res, next) {
-  res.render('fronts/contact.ejs', { title: 'Express' });
+  res.render('fronts/contact.ejs', { page: 'contact' });
 });
 router.get('/blog', function(req, res, next) {
-  res.render('fronts/blog.ejs', { title: 'Express' });
+  res.render('fronts/blog.ejs', { page: 'blog' });
+});
+router.get('/shake', function(req, res, next) {
+  res.render('fronts/shake.ejs', { page: 'shake' });
+});
+router.get('/hotdog', function(req, res, next) {
+  res.render('fronts/hotdog.ejs', { page: 'hotdog' });
+});
+router.get('/breakfast', function(req, res, next) {
+  res.render('fronts/breakfast.ejs', { page: 'breakfast' });
 });
 
 module.exports = router;
