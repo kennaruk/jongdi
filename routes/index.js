751c3e4b8417ded21e6681f86eb335bef5e30d01
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2017-05-11 02:36:31 +0700

    index fixed

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,42 @@ var auth = function(req, res, next) {
 };
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('fronts/index.ejs', { page: 'home' });
+  db.getItemLists(function(err, rows, fields) {
+    if(rows.length == 0) {
+      res.render('fronts/null-item.ejs', { page: 'menu', items: rows });
+    } else {
+      var length = rows.length;
+      var first = Math.floor(Math.random() * length);
+      var count = 0;
+      var arr = [];
+      for(var i = 0 ; i < 3 ; i++) {
+        if(count+1 == length)
+          break;
+        while(true) {
+          var random = Math.floor(Math.random() * length);
+          console.log("random = "+random);
+          var notSame = false;
+          for(var j = 0 ; j < arr.length ; j++) {
+            if(random == arr[j])
+              notSame = true;
+          }
+          if(!notSame && random != first) {
+            arr.push(random);
+            count++;
+            break;
+          }
+        }
+      }
+      var first_picture = rows[first].item_picture;
+      var first_id = rows[first].item_id;
+      var bottoms = [];
+      for(var i = 0 ; i < arr.length ; i++) {
+        bottoms.push({ id: rows[arr[i]].item_id, picture: rows[arr[i]].item_picture });
+      }
+      console.log(bottoms);
+    } // end else
+    res.render('fronts/index.ejs', { page: 'home', first_picture: first_picture, first_id: first_id, bottoms: bottoms });
+  });
 });
 router.get('/about', function(req, res, next) {
   res.render('fronts/about.ejs', { page: 'about' });
