802bb6539b665820ba302ac1dae3cf9bca1277d2
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2017-05-11 18:16:11 +0700

    login bug fixed

--- views/fronts/login-session.js
+++ views/fronts/login-session.js
@@ -2,16 +2,22 @@
 <script type="text/javascript">
   $(function () {
 		if('<%= user['status'] %>' == 'log-in-failed') { //logout status
-      $('#log-out').hide();
+      $('#log-out-bttn').hide();
+      $('#log-in-bttn').show();
+      $('#register-bttn').show();
       if('<%= page %>' == 'log-in')
 			   alert('Login failed, please try again.');
 		} else if('<%= user['status'] %>' == 'log-in-success') { //login status
       $('#log-out-bttn').show();
-      $('#log-in').hide();
+      $('#log-in-bttn').hide();
       $('#register').hide();
       // if('<%= page %>' == 'home')
 			//    alert('Login success');
-		}
+		} else if('<%= user['status'] %>' == 'not-login') {
+      $('#log-out-bttn').hide();
+      $('#log-in-bttn').show();
+      $('#register-bttn').show();
+    }
     console.log("status = "+'<%=user['status']%>');
   });
 </script>
